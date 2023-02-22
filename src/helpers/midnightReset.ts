337e6dee1b7adb3241a6535794df66624b0dd4df
dejmedus <email96123@gmail.com>
2023-02-22 00:11:43 -0700

    change instances of var to let

--- src/helpers/midnightReset.ts
+++ src/helpers/midnightReset.ts
@@ -1,7 +1,7 @@
 // https://stackoverflow.com/questions/26306090/running-a-function-everyday-midnight
 export default function resetAtMidnight(reset) {
-  var now = new Date();
-  var night = new Date(
+  let now = new Date();
+  let night = new Date(
     now.getFullYear(),
     now.getMonth(),
     now.getDate() + 1, // the next day, ...
@@ -9,7 +9,7 @@ export default function resetAtMidnight(reset) {
     0,
     0 // ...at 00:00:00 hours
   );
-  var msToMidnight = night.getTime() - now.getTime();
+  let msToMidnight = night.getTime() - now.getTime();
 
   setTimeout(function () {
     reset();
