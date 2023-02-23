85f37e215fcd88de5682c448906d2dab79168e97
dejmedus <email96123@gmail.com>
2023-02-23 00:40:12 -0700

    changed reset back because it was fine in the first place

--- src/helpers/midnightReset.ts
+++ src/helpers/midnightReset.ts
@@ -1,11 +1,14 @@
 // https://stackoverflow.com/questions/26306090/running-a-function-everyday-midnight
 export default function resetAtMidnight(reset) {
   let now = new Date();
-  let night = now;
-  // the next day at 00:00:00 hours
-  night.setDate(now.getDate() + 1);
-  night.setHours(0, 0, 0);
-
+  let night = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() + 1, // the next day, ...
+    0,
+    0,
+    0 // ...at 00:00:00 hours
+  );
   let msToMidnight = night.getTime() - now.getTime();
 
   const timeout = setTimeout(function () {
