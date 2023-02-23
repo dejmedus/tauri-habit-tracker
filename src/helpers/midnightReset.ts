40098bc78fc83768b52346177fdcc99f888b60c6
dejmedus <email96123@gmail.com>
2023-02-23 00:31:07 -0700

    update reset so it doesnt use old year/date info & clear timeout

--- src/helpers/midnightReset.ts
+++ src/helpers/midnightReset.ts
@@ -1,18 +1,16 @@
 // https://stackoverflow.com/questions/26306090/running-a-function-everyday-midnight
 export default function resetAtMidnight(reset) {
   let now = new Date();
-  let night = new Date(
-    now.getFullYear(),
-    now.getMonth(),
-    now.getDate() + 1, // the next day, ...
-    0,
-    0,
-    0 // ...at 00:00:00 hours
-  );
+  let night = now;
+  // the next day at 00:00:00 hours
+  night.setDate(now.getDate() + 1);
+  night.setHours(0, 0, 0);
+
   let msToMidnight = night.getTime() - now.getTime();
 
-  setTimeout(function () {
+  const timeout = setTimeout(function () {
     reset();
     resetAtMidnight(reset); //      Then, reset again next midnight.
   }, msToMidnight);
+  () => clearTimeout(timeout);
 }
