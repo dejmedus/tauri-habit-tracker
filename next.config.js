9f7034cb971129a8c3d0f4deeb156142d92e8976
Julia <email96123@gmail.com>
2024-02-27 09:38:17 -0700

    update export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;
